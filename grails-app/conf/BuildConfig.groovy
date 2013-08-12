487eb8112e6b472517f501f422fba8e9528e775a
Daniel Nesbitt <dnesbitt@genedata.com>
2013-08-12 12:55:48 -0400

    Add development configuration settings.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,7 +22,8 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-//grails.plugin.location.rmodules = "C:\\SVN\\repo1\\pharma\\transmart\\trunk\\plugins\\Rmodules"
+grails.plugin.location.rmodules = "../Rmodules"
+grails.plugin.location.gexModule = "../tmart-gex"
 
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
