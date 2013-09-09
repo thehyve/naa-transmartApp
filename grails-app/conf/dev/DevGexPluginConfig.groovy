48de4bda839083f6a8cea7545f6aa4e03805571b
Scott Faria <scott.faria@genedata.com>
2013-09-09 15:30:25 -0400

    Configuration for Analyst invocation.

--- grails-app/conf/dev/DevGexPluginConfig.groovy
+++ grails-app/conf/dev/DevGexPluginConfig.groovy
@@ -5,10 +5,12 @@ Gex {
     // working area for exported files
 	tempFolderDirectory = "C:\\Users\\" + System.getProperty("user.name") + "\\AppData\\Local\\Temp\\jobs\\"
 
-    // status messages for quartz
-    statusList = [
-        "Started",
-        "Gathering Data",
-        "Sending Data To Expressionist"
-    ]
+    // parameters for invoking Genedata Analyst
+    analystServerHostname = ""
+    analystServerPort = -1
+    analystLoaderID = "transmart"
+
+    // Analyst Loader URL - Do not manually configure!
+    analystLoaderBaseURL = "http://${analystServerHostname}:${analystServerPort}/analyst/loader"
+
 }
