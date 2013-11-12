b66f8080fd44cee4ecf81c7a17e33176d4094fa3
akhandeshi <ami.engineer@gmail.com>
2013-11-12 12:00:42 -0500

    Added build path...this is test

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,8 +22,8 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-grails.plugin.location.rmodules = "../Rmodules"
-grails.plugin.location.gexModule = "../tmart-gex"
+grails.plugin.location.rmodules = "../../Rmodules"
+grails.plugin.location.gexModule = "../../tmart-gex"
 
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
