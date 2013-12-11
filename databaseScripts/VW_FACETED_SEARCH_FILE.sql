ff73a24d15179a367d0c0d60b1c9499e3f23b092
davidxn <davidknewton@gmail.com>
2013-12-11 11:19:45 -0800

    Adding HG19 MV and SEARCH_FILE script

--- databaseScripts/VW_FACETED_SEARCH_FILE.sql
+++ databaseScripts/VW_FACETED_SEARCH_FILE.sql
@@ -1,20 +1,16 @@
-CREATE OR REPLACE VIEW "BIOMART"."VW_FACETED_SEARCH_FILE" AS
-select f.folder_id as FOLDER_ID, fa.object_uid as OBJECT_UID, bio_data_id as STUDY_ID, x.filenames as FILE_TEXT
+CREATE OR REPLACE VIEW BIOMART.VW_FACETED_SEARCH_FILE AS
+select f.folder_id as FOLDER_ID
+, fa.object_uid as OBJECT_UID
+, bio_data_id as STUDY_ID
+, trim(leading ' ' from listagg(to_char(fi.display_name || ' ' || fi.description),' ') within group (order by fi.display_name)) as filenames
 from fmapp.fm_folder f
-inner join fmapp.fm_folder_association fa on fa.folder_id = f.folder_id
-inner join biomart.bio_data_uid buid ON fa.object_uid = buid.unique_id
-inner join biomart.bio_experiment be ON buid.bio_data_id = be.bio_experiment_id
-left outer join
-  (select id, filenames from
-    (select
-      ff.folder_id as id, fi.display_name || ' ' || fi.description as filename, 'FILE' as row_type
-      FROM fmapp.fm_folder ff
-      JOIN fmapp.fm_folder_file_association ffa on ff.folder_id = ffa.folder_id
-      JOIN fmapp.fm_file fi on fi.file_id = ffa.file_id
-    )
-    pivot (
-      listagg(to_char(filename), ' ') within group (order by filename)
-      for row_type in ('FILE' as filenames)
-    )
-  ) x on x.id = f.folder_id
-;
+inner join fmapp.fm_folder_association fa on f.folder_id = fa.folder_id
+inner join biomart.bio_data_uid buid 
+ON fa.object_uid = buid.unique_id
+inner join biomart.bio_experiment be 
+ON buid.bio_data_id = be.bio_experiment_id
+left outer join fmapp.fm_folder_file_association ffa on f.folder_id = ffa.folder_id
+left outer join fmapp.fm_file fi on fi.file_id = ffa.file_id
+group by f.folder_id
+, fa.object_uid 
+, bio_data_id ;
