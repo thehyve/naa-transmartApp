206e747aadffa3ad2c28c7099eebc21ba4debbec
akhandeshi <EnginA01@AMRCEWLR9EG45A.amer.pfizer.com>
2013-12-17 13:34:41 -0500

    removed commented out code

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,8 +22,6 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-//grails.plugin.location.rmodules = "../Rmodules"
-//grails.plugin.location.gexModule = "../tmart-gex"
 
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
