d593e04d8b39766b5afed6826db73e810e121bcc
Florian Guitton <f.guitton@imperial.ac.uk>
2014-11-11 18:16:43 +0000

    Addressing issue TMART-357. I have removed the basicGrid methods too, it is useless ...
    (cherry picked from commit 582c7ce4111eba7742420e175302f5b786f288da)

--- web-app/js/datasetExplorer/datasetExplorer.js
+++ web-app/js/datasetExplorer/datasetExplorer.js
@@ -3530,7 +3530,7 @@ function getSummaryGridData() {
 
     gridstore = new Ext.data.JsonStore(
         {
-            url : pageInfo.basePath+'/chart/basicGrid',
+            url : pageInfo.basePath+'/chart/analysisGrid',
             root : 'rows',
             fields : ['name', 'url']
         }
@@ -3540,7 +3540,6 @@ function getSummaryGridData() {
 
     var myparams = Ext.urlEncode(
         {
-            charttype : "basicgrid",
             concept_key : "",
             result_instance_id1 : GLOBAL.CurrentSubsetIDs[1],
             result_instance_id2 : GLOBAL.CurrentSubsetIDs[2]
