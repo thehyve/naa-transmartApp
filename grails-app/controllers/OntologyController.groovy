3ae5512ea2f7082ffd32b9a58c8aa2d6fc01296c
Ruslan Forostianov <ruslan@thehyve.nl>
2014-12-09 16:18:39 +0100

    Fix metadata popup error
    Also render both tables for study node
    at the same time:
        for study metadata and for i2b2 tags.

--- grails-app/controllers/OntologyController.groovy
+++ grails-app/controllers/OntologyController.groovy
@@ -32,76 +32,59 @@ class OntologyController {
     }
     
     def ajaxOntTagFilter =
-    {
-    		log.trace("called ajaxOntTagFilter")
-    		log.trace("tagterm:"+params.tagterm)
-            def tagterm = params.tagterm
-            def ontsearchterm = params.ontsearchterm
-            def tagtype = params.tagtype
-            def result = ontologyService.searchOntology(tagterm, [ontsearchterm], tagtype, 'JSON')
-    		render result as JSON		
-    }
-    
-    
-    def getInitialSecurity=
-    {
-    		def user = AuthUser.findByUsername(springSecurityService.getPrincipal().username)
-    		def result=i2b2HelperService.getAccess(i2b2HelperService.getRootPathsWithTokens(), user);
-    		render result as JSON
-    }
-    def sectest=
-    {
-    		log.trace("KEYS:"+params.keys)
-    		def keys=params.keys.toString().split(",");
-    		def paths=[];
-    		def access;
-            if (params.keys != "") {
-    		keys.each{ key ->
-    			log.debug("in LOOP")
-    			paths.add(i2b2HelperService.keyToPath(key))	
-    		}	
-    		def user = AuthUser.findByUsername(springSecurityService.getPrincipal().username)
-    	
-    		
-    		 access=i2b2HelperService.getConceptPathAccessCascadeForUser(paths, user)
-    		}
-    		log.trace(access as JSON)
-    }
-	
-	def showConceptDefinition =
-	{
-		def conceptPath=i2b2HelperService.keyToPath(params.conceptKey);
-		def node=i2b2.OntNode.get(conceptPath);
+            {
+                log.trace("called ajaxOntTagFilter")
+                log.trace("tagterm:" + params.tagterm)
+                def tagterm = params.tagterm
+                def ontsearchterm = params.ontsearchterm
+                def tagtype = params.tagtype
+                def result = ontologyService.searchOntology(tagterm, [ontsearchterm], tagtype, 'JSON')
+                render result as JSON
+            }
 
-            //Disabled check for trial - show all study metadata in the same way as the Browse view
-		//def testtag=new i2b2.OntNodeTag(tag:'test', tagtype:'testtype');
-		//node.addToTags(testtag);
-		//node.save();
-//		def trial=node.tags.find{ w -> w.tagtype =="Trial" }
-//		if(trial!=null)
-//		{
-//			def trialid=trial.tag;
-//			chain(controller:'trial', action:'trialDetailByTrialNumber', id:trialid)
-//		}
-            //Check for study by visual attributes
-            if (node.visualattributes.contains("S")) {
-                def accession = node.sourcesystemcd
-                def study = Experiment.findByAccession(accession?.toUpperCase())
-                def folder
-                if (study) {
-                    folder = FmFolderAssociation.findByObjectUid(study.getUniqueId().uniqueId)?.fmFolder
-                } else {
-                    render(status: 200, text: "No study definition found for accession: " + accession)
-            return
-        }
 
+    def getInitialSecurity =
+            {
+                def user = AuthUser.findByUsername(springSecurityService.getPrincipal().username)
+                def result = i2b2HelperService.getAccess(i2b2HelperService.getRootPathsWithTokens(), user);
+                render result as JSON
+            }
+    def sectest =
+            {
+                log.trace("KEYS:" + params.keys)
+                def keys = params.keys.toString().split(",");
+                def paths = [];
+                def access;
+                if (params.keys != "") {
+                    keys.each { key ->
+                        log.debug("in LOOP")
+                        paths.add(i2b2HelperService.keyToPath(key))
+                    }
+                    def user = AuthUser.findByUsername(springSecurityService.getPrincipal().username)
+
+
+                    access = i2b2HelperService.getConceptPathAccessCascadeForUser(paths, user)
+                }
+                log.trace(access as JSON)
+            }
+
+    def showConceptDefinition = {
+        def conceptPath = i2b2HelperService.keyToPath(params.conceptKey)
+        def node = OntNode.get(conceptPath)
+
+        //Check for study by visual attributes
+        if (node.visualattributes.contains("S")) {
+            def accession = node.sourcesystemcd
+            def study = Experiment.findByAccession(accession?.toUpperCase())
+            if (study) {
+                def folder = FmFolderAssociation.findByObjectUid(study.getUniqueId().uniqueId)?.fmFolder
                 def amTagTemplate = amTagTemplateService.getTemplate(folder.getUniqueId())
                 List<AmTagItem> metaDataTagItems = amTagItemService.getDisplayItems(amTagTemplate.id)
 
                 render(template: 'showStudy', model: [folder: folder, bioDataObject: study, metaDataTagItems: metaDataTagItems])
-            } else {
-                render(template: 'showDefinition', model: [tags: node.tags])
-		}
-	}
-	
+            }
+        }
+        render(template: 'showDefinition', model: [tags: node.tags])
+    }
+
 }
