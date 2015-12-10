edf9e0eec54a421615472602e37ad5aecdeeb8f7
Peter Kok <peter@thehyve.nl>
2015-12-10 17:10:25 +0100

    Add notice on code quality

--- grails-app/controllers/ChartController.groovy
+++ grails-app/controllers/ChartController.groovy
@@ -283,6 +283,7 @@ class ChartController {
                 if (s1) i2b2HelperService.addConceptDataToTable(table, ck, result_instance_id1);
                 if (s2) i2b2HelperService.addConceptDataToTable(table, ck, result_instance_id2);
 
+                //TODO: implement check for highdim data in less hacky manner
                 if (filters) {
                     chartService.addHighDimDataToTable(table, ck, chartService.getSubsetsFromRequest(params), filters)
                 }
