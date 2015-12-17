thehyve/naa-transmartApp

commit 65498ae7c02881663239112b376d86e2e27fa0fd

Fixed the titles in summary statistics for SNP data.
Fixed the titles of tables and plots in summary
statistics for high dimensional data.
Previously all charts for a data type would get the
same title, instead of the row label.

Fixes NAA-84.

grails-app/services/ChartService.groovy
@@ -183,50 +183,50 @@ class ChartService {
         def metadata = exportMetadataService.getHighDimMetaData(subsets[1]?.instance as Long, subsets[2]?.instance as Long)
         metadata.each { data ->
             log.debug "getHighDimAnalysis: concept = ${concept}, dataType = ${data.datatype}"
-            def barChartData = [:]
-            subsets.each { k, v ->
-                if (v.instance) {
+            Map<String, Map<String, Map>> barChartData = [:] // maps subsets to a map from row label to row data
+            subsets.each { subset, properties ->
+                if (properties.instance) {
                     def assayConstraints = [
                             (AssayConstraint.PATIENT_SET_CONSTRAINT):
-                                [[result_instance_id: v.instance]],
+                                [[result_instance_id: properties.instance]],
                             (AssayConstraint.ONTOLOGY_TERM_CONSTRAINT):
                                 [[concept_key: concept]]
                         ]
-                    barChartData[k] = highDimChartDataService.getBarChartData(data.datatype.dataTypeName, assayConstraints, filters)
+                    barChartData[subset] = highDimChartDataService.getBarChartData(data.datatype.dataTypeName, assayConstraints, filters)
                 }
             }
-            def rowCount = barChartData.max { it.value.data.size() }.value.data.size()
-            log.debug "Row count: ${rowCount}"
+            List<String> rows = barChartData.collect { it.value.collect { it.key } }.flatten().unique()
             // Rows are the different selected high dimension data rows
-            (0..(rowCount > 0 ? rowCount-1 : 0)).each { i ->
-                def title = ''
+            rows.each { rowName ->
+                log.debug "Row: ${rowName}"
                 Set<String> labels = []
                 def series = []
                 def bardata = [:]
-                barChartData.each { k, v ->
-                    if (i < v.data.size()) {
-                        title = v.title
-                        labels.addAll(v.labels)
-                        series.add("Subset ${k}")
-                        bardata[k] = v.data[i]
+                barChartData.each { subset, Map<String, Map> rowMap ->
+                    if (rowMap[rowName]) {
+                        Map row = rowMap[rowName]
+                        log.debug "Subset: ${subset}, row: ${row}"
+                        labels.addAll(row.labels)
+                        series.add("Subset ${subset}")
+                        bardata[subset] = row.data
                     }
                 }
                 List labelList = new ArrayList(labels)
                 labelList.sort()
-                Map sortedData = bardata.collectEntries { k, v ->
-                    SortedMap d = new TreeMap()
+                Map sortedData = bardata.collectEntries { subset, rowData ->
+                    LinkedHashMap sortedRowData = [:]
                     labelList.each { label ->
-                        d[label] = v[label] ?: 0
+                        sortedRowData[label] = rowData[label] ?: 0
                     }
-                    [(k): d]
+                    [(subset): sortedRowData]
                 }
-                log.debug "Title: ${title}, plot data: ${sortedData}"
+                log.debug "Title: ${rowName}, plot data: ${sortedData}"
                 result << [
-                    plots: sortedData.collectEntries { k, v ->
-                        def plottitle = title //"Genotype distribution in subset $k for SNP $title"
-                        [(k): getSVGChart(type: 'bar', title: "$plottitle", data: v, size: chartSize)]
+                    plots: sortedData.collectEntries { subset, rowData ->
+                        def plottitle = rowName //"Genotype distribution in subset $k for SNP $rowName"
+                        [(subset): getSVGChart(type: 'bar', title: "$plottitle", data: rowData, size: chartSize)]
                     },
-                    title: title,
+                    title: rowName,
                     labels: labelList,
                     series: series,
                     data: sortedData,

grails-app/services/org/transmartproject/HighDimChartDataService.groovy
@@ -75,7 +75,7 @@ class HighDimChartDataService {
      *
      * @param dataType highdim data type
      */
-    Map getBarChartData(
+    Map<String, Map> getBarChartData(
                 String dataType,
                 Map assayConstraintsSpec,
                 List filters) {
@@ -101,26 +101,17 @@ class HighDimChartDataService {
         TabularResult tabularResult = typeResource.retrieveData(
                 assayConstraints, [dataConstraints], projection)
 
-        List<Map> barChartData = []
-        Set<String> labels = []
+        Map<String, Map> barChartData = [:]
         try {
             for (Object row: tabularResult) {
-                if (!metadata) {
-                    metadata = getBarChartMetadataForDatatype(typeResource, row)
-                }
                 Map rowData = transformRow(row)
-                labels.addAll(rowData.keySet())
-                barChartData.add(rowData)
+                barChartData[row.label] = [title: row.label, labels: rowData.keySet(), data: rowData]
             }
         } finally {
             tabularResult.close() //closing the tabular result, no matter what
         }
 
-        [
-            title: metadata.title,
-            labels: labels,
-            data: barChartData
-        ]
+        barChartData
     }
 
     /**

commit 17ff2894e9316cde48b2d80d400a06c35efbfddb

Show message when no data found again.
This was broken in 65498ae7c02

Fixes issue #NAA-83

grails-app/services/ChartService.groovy
@@ -236,6 +236,14 @@ class ChartService {
                     filters: filters
                 ]
             }
+            if (rows.empty) {
+                result << [
+                    concept: concept,
+                    dataType: data.datatype.dataTypeName,
+                    dataTypeDescription: data.datatype.dataTypeDescription,
+                    filters: filters
+                ]
+            }
         }
         result
     }

grails-app/views/chart/_highdimComparison.gsp
@@ -1,22 +1,24 @@
 
 <div class="highdimComparison">
     <g:each in="${subsets?.commons?.highdim}" var="highdim">
-        <div class="plotrow">
-            <div class="plotcolumn">
-            <div class="plotbox">
-                ${highdim.plots[1]}
+        <g:if test="${highdim.plots != null}">
+            <div class="plotrow">
+                <div class="plotcolumn">
+                <div class="plotbox">
+                    ${highdim.plots[1]}
+                </div>
+                </div>
+                <div class="plotcolumn">
+                <div class="plotbox">
+                <g:if test="highdim.plots[2]">
+                    ${highdim.plots[2]}
+                </g:if>
+                </div>
+                </div>
             </div>
-            </div>
-            <div class="plotcolumn">
-            <div class="plotbox">
-            <g:if test="highdim.plots[2]">
-                ${highdim.plots[2]}
-            </g:if>
-            </div>
-            </div>
-        </div>
-        <div style="clear: both;"></div>
-        <g:if test="${highdim.data.size() == 0}">
+            <div style="clear: both;"></div>
+        </g:if>
+        <g:if test="${highdim.data == null}">
             <div>No data of type ${highdim.dataTypeDescription} found for ${highdim.concept}.</div>
             <g:if test="${highdim.filters.size() > 0}">
                 <g:each in="${highdim.filters}" var="filter">